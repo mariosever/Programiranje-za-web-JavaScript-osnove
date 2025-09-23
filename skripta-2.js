92e71d30e5172234ff7f6ef25601168d31932445
Mario Sever <34382956+mariosever@users.noreply.github.com>
2025-09-23 09:16:42 +0200

    Add files via upload

---
+++ skripta-2.js
@@ -0,0 +1,23 @@
+// Varijable
+let ime = "Ivo";
+let prezime = "Ivić";
+let a = 2;
+let b = 5;
+let c = a + b;
+
+// Konstante
+const pi = 3.14;
+const oib = 12345678991;
+
+// Ispis varijabli u konzoli
+console.log(ime);
+console.log(prezime);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(pi);
+console.log(oib);
+
+// Ispis na web stranici
+document.getElementById("ispis").innerHTML = ime;
+
