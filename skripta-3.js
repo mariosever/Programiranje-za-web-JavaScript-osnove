aede5af4c2672c457ea0119939f58f0e6078f3c1
Mario Sever <34382956+mariosever@users.noreply.github.com>
2025-10-01 11:06:25 +0200

    Add files via upload

---
+++ skripta-3.js
@@ -0,0 +1,46 @@
+// Varijable i tipovi podataka
+
+// Brojevi
+let a = 83; // Int
+let b = 7;
+
+let duljina = 72.38; // Float
+let tezina = 123.88;
+
+// Konstanta
+const PI = 3.14; 
+
+// Nizovi znakova
+let ime = "Filip Filiphovich";
+let poruka = "Pozdrav hakeri!";
+
+// Polja (Array)
+let auti = ["bmw", "audi", "mercedes"];
+
+// Objekti
+const osoba = {ime: "Ivo", prezime: "Ivić", razred: "3.a"};
+
+// Vježba: Ispis u konzolu
+console.log(a);
+console.log(b);
+
+console.log(duljina);
+console.log(tezina);
+
+console.log(ime);
+console.log(poruka);
+console.log(PI);
+
+console.log(auti[0]);
+console.log(auti[1]);
+console.log(auti[2]);
+
+console.log(osoba.ime);
+console.log(osoba.prezime);
+console.log(osoba.razred);
+
+
+
+
+
+
